76bcbcbbfabbd2e743c0ab25eaa47c13db7f21ad
Open Markets Initiative <contact@openmarketsinitiative.com>
2026-09-17 12:34:08 -0500

    Regenerate with Eurex tests

--- .github/tests/test_eurex_t7_eti_v10_0.py
+++ .github/tests/test_eurex_t7_eti_v10_0.py
@@ -12,11 +12,11 @@ import payloads
 SPICY_DRIVER = os.environ.get("SPICY_DRIVER", "spicy-driver")
 
 
-class EurexT7XtiV100Tests(unittest.TestCase):
+class EurexT7EtiV100Tests(unittest.TestCase):
 
     def test_orderexecresponse(self):
-        module = "eurex/t7/xti/eurex_t7_xti_v10_0_server.spicy"
-        for payload in payloads.of("omi-data-packets/Eurex/T7.Xti.Fbe.v10.0/OrderExecResponse.pcap"):
+        module = "eurex/t7/eti/eurex_t7_eti_v10_0_server.spicy"
+        for payload in payloads.of("omi-data-packets/Eurex/T7.Eti.Fbe.v10.0/OrderExecResponse.pcap"):
             result = subprocess.run([SPICY_DRIVER, module], input=payload, capture_output=True)
             self.assertEqual(result.returncode, 0, result.stderr.decode())
 
